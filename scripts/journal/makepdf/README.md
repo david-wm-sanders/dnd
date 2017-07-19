187b278a38af7c7a7080f70d4f19fb448bae8371
David Sanders <david.wm.sanders@gmail.com>
2017-07-19 21:03:22 +0100

    Add journal-makepdf script

---
+++ scripts/journal/makepdf/README.md
@@ -0,0 +1,24 @@
+# README
+## Setup:
+### Windows (x64):
+0. Create a venv:  
+`python.exe -m venv venv/`
+0. Upgrade setuptools in the venv with:  
+` .\venv\Scripts\pip.exe install --upgrade setuptools`  
+This step is essential to allow distutils to find the toolchain installed by Microsoft Build Tools for Visual Studio 2017.
+0. Install the weasyprint requirements as instructed at  
+`http://weasyprint.readthedocs.io/en/latest/install.html#windows`  
+ 1. Install GTK3 with "Set up PATH environment variable to include GTK+" checked from:  
+ `https://github.com/tschoonj/GTK-for-Windows-Runtime-Environment-Installer/releases`
+ 2. Restart machine
+ 3. Install Build Tools for Visual Studio 2017 from:  
+ `https://www.visualstudio.com/downloads/#build-tools-for-visual-studio-2017`
+0. `pip install markdown weasyprint`
+0. Download the lxml‑3.8.0‑cp36‑cp36m‑win_amd64.whl from:  
+`http://www.lfd.uci.edu/~gohlke/pythonlibs/`
+0. Uninstall the *broken* version of lxml from the venv with:  
+`.\venv\Scripts\pip.exe uninstall lxml`
+0. Install lxml from the wheel with:  
+`.\venv\Scripts\pip.exe install .\wheels\lxml-3.8.0-cp36-cp36m-win_amd64.whl`
+0. Test the weasyprint install with:  
+`python -m weasyprint http://weasyprint.org weasyprint.pdf`
---
+++ scripts/journal/makepdf/makepdf.py
@@ -0,0 +1,43 @@
+import markdown
+import weasyprint
+from pathlib import Path
+
+# name = input("Character name: ")
+# journal_path = Path(f"../../../campaigns/prydain/pc/{name}/journal/")
+
+journal_path = Path(f"../../../campaigns/prydain/pc/durg/journal/")
+
+# if not journal_path.exists():
+#     raise Exception(f"No journal found for a character named '{name}'")
+
+exclusions = ["1742"]
+md_blocks = ["# The Journal of Durg, son of Darg\n"]
+
+print(f"\nOpening journal at '{journal_path}'")
+year_paths = [p for p in journal_path.iterdir() if p.is_dir()]
+for year_path in year_paths:
+    print(f"Processing entries for {year_path.name}...")
+    md_blocks.append(f"# {year_path.name}\n")
+    month_paths = [p for p in year_path.iterdir() if p.is_dir()]
+    for month_path in month_paths:
+        month_name = month_path.name.split("_")[1].capitalize()
+        print(f" Processing entries for {month_name}...")
+        md_blocks.append(f"## {month_name}\n")
+        entries = month_path.glob("*.md")
+        for entry in entries:
+            for exclusion in exclusions:
+                if exclusion in str(entry):
+                    print(f"  Entry in '{entry.name}' excluded because '{exclusion}' is in the exclusions list.")
+                    md_blocks.append(f"* Excluded '{entry.name}' because '{exclusion}' is in the exclusions list.\n")
+                    break
+            else:
+                with entry.open(encoding="utf-8") as f:
+                    md = f.read()
+                    md_blocks.append(md)
+
+print("\nCollating markdown blocks into journal text...")
+journal_text = "".join(md_blocks)
+print("Converting journal text into HTML...")
+html = markdown.markdown(journal_text, extensions=['markdown.extensions.extra'])
+print("Rendering HTML to journal.pdf...")
+weasyprint.HTML(string=html).write_pdf("journal.pdf")
