848904b79601c0b2faa7745b503603141a1ee3a3
David Sanders <david.wm.sanders@gmail.com>
2018-02-11 22:08:39 +0000

    Add more functionality to xgmchargen

--- scripts/xgmchargen/xgmchargen.py
+++ scripts/xgmchargen/xgmchargen.py
@@ -4,6 +4,8 @@ import json
 import random
 from pathlib import Path
 
+import dice
+
 
 data_p = Path(__file__).parent / Path("data")
 
@@ -28,26 +30,32 @@ def load_roll_data(table_name):
             weights.append(1 / dice)
 
     # print(list(zip(population, weights)))
-    # print(sum(weights))
+    print(f">> {sum(weights)}")
     return population, weights
 
 
-def roll_parents():
-    table_name = "parents"
+def roll_from_table(table_name):
     population, weights = load_roll_data(table_name)
     result = random.choices(population, weights)
-    print("Parents:")
-    print(f" - You {result[0]} who your parents are or were.")
     return result[0]
 
 
-def roll_birthplace():
-    table_name = "birthplace"
-    population, weights = load_roll_data(table_name)
-    result = random.choices(population, weights)
-    print("Birthplace:")
-    print(f" - You were born {result[0]}.")
-    return result[0]
+def generate_origins():
+    race = roll_from_table("race")
+    parents_known = roll_from_table("parents")
+    birthplace = roll_from_table("birthplace")
+    print("Origins:")
+    print(f" - You are {race}.")
+    if race in ["a half-elf", "a half-orc", "a tiefling"]:
+        if race == "a half-elf":
+            parent_races = roll_from_table("halfelfparents")
+        elif race == "a half-orc":
+            parent_races = roll_from_table("halforcparents")
+        elif race == "a tiefling":
+            parent_races = roll_from_table("tieflingparents")
+        print(f"  - Your parents were {parent_races}.")
+    print(f" - You {parents_known} who your parents are or were.")
+    print(f" - You were born {birthplace}.")
 
 
 if __name__ == '__main__':
@@ -56,5 +64,4 @@ if __name__ == '__main__':
     #     restest.append(roll_parents())
     # counter = collections.Counter(restest)
     # print(counter.most_common())
-    roll_parents()
-    roll_birthplace()
+    generate_origins()
