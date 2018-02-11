dc088f286b14265d7d28888882ab5938efcedd47
David Sanders <david.wm.sanders@gmail.com>
2018-02-11 23:37:28 +0000

    Add siblings and family

--- scripts/xgmchargen/xgmchargen.py
+++ scripts/xgmchargen/xgmchargen.py
@@ -19,8 +19,8 @@ def load_roll_data(table_name):
 
     dice = j["dice"]
     for roll in j["rolls"]:
-        population.append(roll["result"])
-        r = roll["roll"]
+        r = roll.pop("roll")
+        population.append(roll)
         if "-" in r:
             rl, ru = r.split("-")
             rl, ru = int(rl), int(ru)
@@ -36,16 +36,24 @@ def load_roll_data(table_name):
 
 def roll_from_table(table_name):
     population, weights = load_roll_data(table_name)
-    result = random.choices(population, weights)
-    return result[0]
+    res = random.choices(population, weights)[0]
+    if "result" in res:
+        return res["result"]
+    elif "result_diceroll" in res:
+        x = dice.roll(res["result_diceroll"])
+        return x
+    elif "result_template" in res:
+        pass
+    else:
+        raise Exception("What?! No valid result?")
 
 
 def generate_origins():
-    race = roll_from_table("race")
-    parents_known = roll_from_table("parents")
-    birthplace = roll_from_table("birthplace")
     print("Origins:")
+    # Roll a race
+    race = roll_from_table("race")
     print(f" - You are {race}.")
+    # If half-blood race, determine what each of the parents were
     if race in ["a half-elf", "a half-orc", "a tiefling"]:
         if race == "a half-elf":
             parent_races = roll_from_table("halfelfparents")
@@ -54,8 +62,19 @@ def generate_origins():
         elif race == "a tiefling":
             parent_races = roll_from_table("tieflingparents")
         print(f"  - Your parents were {parent_races}.")
+    # Roll to discover if parents were known or not
+    parents_known = roll_from_table("parents")
     print(f" - You {parents_known} who your parents are or were.")
+    # Roll up a birthplace
+    birthplace = roll_from_table("birthplace")
     print(f" - You were born {birthplace}.")
+    # Roll the number of siblings
+    sibling_number = roll_from_table("siblingnumber")
+    print(f" - You have {sibling_number} sibling(s).")
+    # TODO: Roll sibling gender, age?, occupation, alignment, status,
+    #       relationship, et cetera
+    family = roll_from_table("family")
+    print(f" - You were raised {family}.")
 
 
 if __name__ == '__main__':
